2f428f4cd067f77ea0bcd14809a8a3a647adf7ec
LAPTOP-BT7A7TCK\admin <Youbelongwithme29@gmail.com>
2026-04-02 16:25:24 +0700

    feat(enemy): add basic movement logic

---
+++ Enemy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        // Tính năng: Quái vật di chuyển về phía trước
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
