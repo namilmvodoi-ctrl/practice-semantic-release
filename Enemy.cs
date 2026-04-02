a34d1c5c5dd8cca4823a60539a7239677fecb113
LAPTOP-BT7A7TCK\admin <Youbelongwithme29@gmail.com>
2026-04-02 17:21:14 +0700

    feat(die)! : add a die function

--- Enemy.cs
+++ Enemy.cs
@@ -9,9 +9,14 @@ public class EnemyMovement : MonoBehaviour
         // Tính năng: Quái vật di chuyển về phía trước
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
-    public void TakeDamage(int damage)
+    public void TakeDamage(float damage)
     {
         // Tính năng: Quái vật nhận sát thương
         // Giảm máu của quái vật và kiểm tra nếu nó chết
+    }
+    public void Die()
+    {
+        // Tính năng: Quái vật chết
+        // Phát hiệu ứng chết và hủy đối tượng quái vật
     }
 }
